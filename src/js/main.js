2b1af6a9c5a2c9cbeecda1ae19846c8fb41cba61
nafijfuad39 <nafijfuad39@gmail.com>
2025-06-05 08:33:28 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle_menu').addEventListener('click',()=>{
+            const mobileMenu = document.getElementById('mobile_menu');
+            mobileMenu.classList.toggle('hidden');
+})
